cade174c6d6a3d7258e7cd9206c6067328942f58
Nilseu Padilha <nilseu.junior@aurea.com>
2021-09-20 21:12:35 +0100

    rgn vertx in action

--- rgn-vertx-inaction/ch02/src/main/java/hello/HelloVerticle.java
+++ rgn-vertx-inaction/ch02/src/main/java/hello/HelloVerticle.java
@@ -1,2 +1,35 @@
-package hello;public class HelloVerticle {
+package hello;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloVerticle extends AbstractVerticle {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloVerticle.class);
+    private long counter = 1;
+
+
+    @Override
+    public void start() throws Exception {
+        vertx.setPeriodic(5_000, id  -> {
+            LOGGER.info("tick");
+        });
+
+        vertx.createHttpServer()
+                .requestHandler(request -> {
+                    LOGGER.info("Request #{} from {}", counter ++, request.remoteAddress().host());
+                    request.response().end("Hello!");
+
+                })
+        .listen(8080);
+        LOGGER.info("Open http://localhost:8080/");
+
+    }
+
+    public static void main(String[] args) {
+        final var vertx = Vertx.vertx();
+        vertx.deployVerticle(new HelloVerticle());
+    }
 }
